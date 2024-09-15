c2dc545458a21c5aa0bef914a56b19c2676d2a19
Tyuiu.TalalaevaAV@outlook.com <sashatalaaleva4@gmail.com>
2024-09-15 22:38:01 +0500

    Task4

---
+++ Tyuiu.TalalaevaAV.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+using Tyuiu.TalalaevaAV.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.TalalaevaAV.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtaction(10, 5));
+        }
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
+    }
+}
